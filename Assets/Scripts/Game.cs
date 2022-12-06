95684496f01ab25fccb8274466d9c68e725b3dd6
Radostny <a.radostny@gmail.com>
2022-12-07 01:03:25 +0500

    Add click on Card with RayCasting

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -6,9 +6,24 @@ public class Game : MonoBehaviour
 {
     [SerializeField]
     private Table _table;
-    // Start is called before the first frame update
+    [SerializeField] Camera _camera;
+    private Ray TouchRay => _camera.ScreenPointToRay(Input.mousePosition);
+    private RaycastHit _hit;
+
     void Start()
     {
         _table.Initialize();
+    }
+
+    private void Update()
+    {
+        Debug.DrawRay(_camera.transform.position, Vector3.forward, Color.yellow);
+        if (Physics.Raycast(TouchRay, out _hit))
+        {
+            if (_hit.collider.gameObject.GetComponentInParent<Card>())
+            {
+                Debug.Log(_hit.collider.gameObject.GetComponentInParent<Card>().Rank);
+            }
+        }
     }
 }
